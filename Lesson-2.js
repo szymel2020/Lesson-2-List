7e9b09505835c7fe9eed8528457878dd56e36a18
szymel2020 <szymon.chala@gmail.com>
2020-12-18 22:24:56 +0100

    add clock

--- Lesson-2.js
+++ Lesson-2.js
@@ -53,7 +53,7 @@ function runPlus(e) {
 	li.appendChild(deleteBtn);
 
 	// Append 'li' to List and change color 'li'
-	var colorFonts = ['pink', '#000000', '#fd0303', '#b12323', '#fc0ef3', '#7b7b7b', '#870def', '#9b7ab7', '#19a7a7', '#3dffff', '#054646', '#04f256', '#36b562', '#076528', '#00ff00', '#257225', '#80cc80', '#ebff00', '#8a9509', '#bdc659', '#bccb12', '#ffc400', '#ebc239', '#98780d', '#524416', '#f73101', '#b43315', '#ea795e', '#eac1b7', '#551202', '#ff0101', '#e31ae3', '#e27ae2', '#530953'];
+	var colorFonts = ['pink', '#000000', '#5d9a79',   '#ee628d',   '#ca719c',   '#fd0303', '#56d1ad',  '#fd0206', '#e054c0',  '#04fa06', '#5d4a45',   '#5d7145', '#7ce41b',  '#040706',   '#b12323', '#739e81',   '#3045e8', '#fc0ef3', '#7b7b7b', '#870def', '#9b7ab8', '#19a7a7', '#3dffff', '#054646', '#04f256', '#36b562', '#076528', '#00ff00', '#257225', '#80cc80', '#ebff00', '#8a9509', '#bdc659', '#bccb12', '#ffc400', '#ebc239', '#98780d', '#524416', '#f73101', '#b43315', '#ea795e', '#eac1b7', '#551202', '#ff0101', '#e31ae3', '#e27ae2', '#530953'];
 	itemList.appendChild(li).style.color = colorFonts[Math.random() * colorFonts.length << 0];
 };
 
@@ -67,6 +67,7 @@ function removeItem(e) {
 	}
 };
 
+// Theme dark
 function pushDark() {
 	let bg = document.getElementById("body");
 	let openList = document.getElementById("openList");
@@ -111,3 +112,40 @@ function pushDark() {
 		//btn5.style.color = "black";
 	};
 };
+
+// Clock
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 24;
+    }
+    
+    if(h > 24){
+        h = h - 24;
+        //session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
+
+
+
+
+
